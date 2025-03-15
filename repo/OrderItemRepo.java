47e29e8475e9b777a9f16e0e574caf34dfda7b26
ushareddyyy <158132529+ushareddyyy@users.noreply.github.com>
2025-03-15 17:33:25 +0530

    Delete repo directory

--- repo/OrderItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.ecommerce.entity.OrderItem;
-@Repository
-public interface OrderItemRepo extends JpaRepository<OrderItem, Integer> {
-}
-
